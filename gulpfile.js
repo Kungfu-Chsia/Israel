a7edeadbf0e9e42dd286cb2e8e5adb29fdfe49df
Kungfu-Chsia <allrighty@ya.ru>
2022-08-08 22:33:36 +0300

    block Programma adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ const sprite = () => {
   return gulp.src('source/img/sprite/*.svg')
       .pipe(svgstore({inlineSvg: true}))
       .pipe(rename('sprite_auto.svg'))
-      .pipe(gulp.dest('build/img'));
+      .pipe(gulp.dest('build/img/sprite'));
 };
 
 const copySvg = () => {
